f80ff305620d24ef1492ac9287442f74ff45ddef
Kate <katensullivan55@gmail.com>
2022-07-08 00:44:03 -0400

    functions to save user tasks and import data

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-var dateDisplay = function(){
+var displayDate = function(){
     var todayDate = moment().format('dddd, MMMM Do YYYY')
     var dayEl = document.getElementById(currentDay)
     dayEl.append(todayDate)
 }
 
-window.addEventListener('load', dateDisplay)
+// shows current date
+window.addEventListener('load', displayDate)
+
+// function that saves tasks
+$("button").on("click", function(){
+    var task = $(this).siblings("textarea").val()
+    console.log(task)
+
+    var taskTime = $(this).siblings(".time-block").attr("id")
+    console.log(taskTime)
+
+    localStorage.setItem(taskTime, task)
+
+    window.alert("Your Task is SAVED to your Scheduler!")
+})
+
+// function that stores data from user input onto page
+$("#nine").siblings("textarea").val(localStorage.getItem("nine"))
+$("#ten").siblings("textarea").val(localStorage.getItem("ten"))
+$("#eleven").siblings("textarea").val(localStorage.getItem("eleven"))
+$("#twelve").siblings("textarea").val(localStorage.getItem("twelve"))
+$("#one").siblings("textarea").val(localStorage.getItem("one"))
+$("#two").siblings("textarea").val(localStorage.getItem("two"))
+$("#three").siblings("textarea").val(localStorage.getItem("three"))
+$("#four").siblings("textarea").val(localStorage.getItem("four"))
+$("#five").siblings("textarea").val(localStorage.getItem("five"))
